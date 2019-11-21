bb366b83f7f5dedff454ea997a32f4a2a25ba039
sunlu <645012844@qq.com>
2019-11-21 15:42:37 +0800

    sun_1.1

---
+++ pages/index/index.vue
@@ -0,0 +1,43 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<view class="main-header-wrap">
+				<span class="xisisi">1111</span>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import 'static/css/index.css'
+	import uniNavBar from 'components/uni-nav-bar/uni-nav-bar.vue'
+	import uniIcon from 'components/uni-icon/uni-icon.vue'
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
