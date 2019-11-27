619ecb2a07d25399855dd632b5921b2c12259fde
sunlu <645012844@qq.com>
2019-11-27 11:11:50 +0800

    my.html

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,20 +2,49 @@
 	<view class="content">
 		<view class="text-area">
 			<view class="main-header-wrap">
-				<span class="xisisi">1111</span>
+				<input type="text" />
+			</view>
+			
+		</view>
+		<view class="uni-padding-wrap main-swiper">
+			<view class="page-section swiper">
+				<view class="page-section-spacing">
+					<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration">
+						<swiper-item>
+							<view class="swiper-item uni-bg-red"><img src="static/images/slide_img1.jpg" height="100%" /></view>
+						</swiper-item>
+						<swiper-item>
+							<view class="swiper-item uni-bg-green"><img src="static/images/slide_img1.jpg" height="100%"/></view>
+						</swiper-item>
+						<swiper-item>
+							<view class="swiper-item uni-bg-blue"><img src="static/images/slide_img1.jpg" height="100%"/></view>
+						</swiper-item>
+					</swiper>
+				</view>
 			</view>
 		</view>
+		<view class="main-two-nav">
+			<ul>
+				<li>11</li>
+				<li>11</li>
+			</ul>
+		</view>
 	</view>
+	
 </template>
 
 <script>
 	import 'static/css/index.css'
-	import uniNavBar from 'components/uni-nav-bar/uni-nav-bar.vue'
-	import uniIcon from 'components/uni-icon/uni-icon.vue'
+	// import uniNavBar from 'components/uni-nav-bar/uni-nav-bar.vue'
+	// import uniIcon from 'components/uni-icon/uni-icon.vue'
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				background: ['color1', 'color2', 'color3'],
+				indicatorDots: true,
+				autoplay: true,
+				interval: 2000,
+				duration: 500
 			}
 		},
 		onLoad() {
---
+++ pages/my/my.vue
@@ -0,0 +1,304 @@
+<template>
+	<view class="content">
+		<view class="my-header-wrap">
+			<img src="static/images/setting-icon.png" alt="" class="setting-icon" />
+			<view class="my-top-txs">
+				<img src="static/images/my-tx.png" alt="" class="my-tx-icon" />
+				<span class="my-name">爱吃面包的猫</span>
+				<span class="my-time">2019-01-02</span>
+			</view>
+		</view>
+		<view class="my-fl-three">
+			<ul>
+				<li>
+					<img src="/static/images/my-icon1.png" alt=""/>
+					<span>创建商区</span>
+				</li>
+				<li>
+					<img src="/static/images/my-icon2.png" alt=""/>
+					<span>发布内容</span>
+				</li>
+				<li>
+					<img src="/static/images/my-icon3.png" alt=""/>
+					<span>付费窗口</span>
+				</li>
+			</ul>
+		</view>
+		<view class="my-cont-wrap">
+			<view class="my-cont-title">
+				<label></label>
+				<span>我的代理信息</span>
+				<img src="static/images/right-arrow.png" alt="" class="right-arrow" />
+			</view>
+			<view class="my-dl-wrap">
+				<ul>
+					<li style="width: 30%;"><span>申请代理商</span></li>
+					<li style="width: 40%;"><span>商家经办加入代理</span></li>
+					<li style="width: 30%;"><span>申请业务员</span></li>
+				</ul>
+			</view>
+		</view>
+		<view class="my-cont-wrap">
+			<view class="my-cont-title">
+				<label></label>
+				<span>我的待审事项</span>
+				<img src="static/images/right-arrow.png" alt="" class="right-arrow" />
+			</view>
+			<view class="my-dl-three">
+				<ul class="my-three-ul">
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>内容投稿待审</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>商区内链待审</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>成员加入待审</label>
+					</li>
+				</ul>
+			</view>
+		</view>
+		<view class="my-cont-wrap">
+			<view class="my-cont-title">
+				<label></label>
+				<span>我的账号管理</span>
+				<img src="static/images/right-arrow.png" alt="" class="right-arrow" />
+			</view>
+			<view class="my-dl-three">
+				<ul class="my-three-ul">
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>我的余额</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>我的支出</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>我的收益</label>
+					</li>
+				</ul>
+			</view>
+		</view>
+		<view class="my-cont-wrap">
+			<view class="my-cont-title">
+				<label></label>
+				<span>我的会员专区</span>
+				<img src="static/images/right-arrow.png" alt="" class="right-arrow" />
+			</view>
+			<view class="my-dl-three">
+				<ul class="my-two-ul">
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>我的内容</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>我的商区</label>
+					</li>
+				</ul>
+			</view>
+		</view>
+		<view class="my-cont-wrap">
+			<view class="my-cont-title">
+				<label></label>
+				<span>我的消息处理</span>
+				<img src="static/images/right-arrow.png" alt="" class="right-arrow" />
+			</view>
+			<view class="my-dl-three">
+				<ul class="my-two-ul">
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>未读消息</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>全部消息</label>
+					</li>
+				</ul>
+			</view>
+		</view>
+		<view class="my-cont-wrap">
+			<view class="my-cont-title">
+				<label></label>
+				<span>我的消息处理</span>
+				<img src="static/images/right-arrow.png" alt="" class="right-arrow" />
+			</view>
+			<view class="my-dl-three">
+				<ul class="my-two-ul">
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>未读消息</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>全部消息</label>
+					</li>
+				</ul>
+			</view>
+		</view>
+		<view class="my-cont-wrap">
+			<view class="my-cont-title">
+				<label></label>
+				<span>我的分享拓客</span>
+				<img src="static/images/right-arrow.png" alt="" class="right-arrow" />
+			</view>
+			<view class="my-dl-three">
+				<ul class="my-three-ul">
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>我的客户</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>拓客提成</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>拓客奖励</label>
+					</li>
+				</ul>
+			</view>
+		</view>
+		<view class="my-cont-wrap">
+			<view class="my-cont-title">
+				<label></label>
+				<span>我的发布内容</span>
+				<img src="static/images/right-arrow.png" alt="" class="right-arrow" />
+			</view>
+			<view class="my-dl-three">
+				<ul class="my-two-ul">
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>我的内容</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>我的投稿</label>
+					</li>
+				</ul>
+			</view>
+		</view>
+		<view class="my-cont-wrap">
+			<view class="my-cont-title">
+				<label></label>
+				<span>我管理的商区</span>
+				<img src="static/images/right-arrow.png" alt="" class="right-arrow" />
+			</view>
+			<view class="my-dl-three">
+				<ul class="my-four-ul">
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>商区</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>内容</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>内链</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>外链</label>
+					</li>
+				</ul>
+			</view>
+		</view>
+		<view class="my-cont-wrap">
+			<view class="my-cont-title">
+				<label></label>
+				<span>我加入的商区</span>
+				<img src="static/images/right-arrow.png" alt="" class="right-arrow" />
+			</view>
+			<view class="my-dl-three">
+				<ul class="my-two-ul">
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>已加商区</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>待审商区</label>
+					</li>
+				</ul>
+			</view>
+		</view>
+		<view class="my-cont-wrap">
+			<view class="my-cont-title">
+				<label></label>
+				<span>我的收藏关注</span>
+				<img src="static/images/right-arrow.png" alt="" class="right-arrow" />
+			</view>
+			<view class="my-dl-three">
+				<ul class="my-two-ul">
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>商区</label>
+					</li>
+					<li>
+						<span>205<img src="static/images/my-wen.png" alt="" class="my-wen-icon" /></span>
+						<label>内容</label>
+					</li>
+				</ul>
+			</view>
+		</view>
+		<view class="my-sj-lx">
+			<view class="my-sj-lx-wrap">
+				<view class="my-cont-title">
+					<span>商九客服信息</span>
+					<img src="static/images/right-arrow.png" alt="" class="right-arrow" />
+				</view>
+				<view class="my-sj-lxx">
+					<view class="my-dh">
+						<span>电话：</span>
+						<label>13500000000</label>
+					</view>
+					<view class="my-yx">
+						<span>邮箱：</span>
+						<label>xxxxxxx@163.com</label>
+					</view>
+				</view>
+			</view>
+		</view>
+		
+	</view>
+	
+</template>
+
+<script>
+	import 'static/css/my.css'
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36px;
+		color: #8f8f94;
+	}
+</style>
