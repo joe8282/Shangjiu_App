390037f06a313df8dba2dd8ef8ff77daa92fd509
sunlu <645012844@qq.com>
2019-12-24 14:47:50 +0800

    提交

--- pages/shopAdd/shopAdd.vue
+++ pages/shopAdd/shopAdd.vue
@@ -17,10 +17,58 @@
 				<view class="shopAdd-cont-title">
 					<span>商号所有者身份：</span>
 				</view>
+				<view v-for="(item , index) in shenfen" @click="changShenfen(index)" :key="index"
+					 :class="{active:a === index}" class="shopAdd-cont-list">
+					<span>{{ item.name }}</span>
+				</view>
 			</view>
 		</view>
-	
-
+		<view class="shopAdd-cont-wrap">
+			<view class="shopAdd-cont-cc">
+				<view class="shopAdd-cont-title">
+					<span>商号行业：</span>
+				</view>
+				<view v-for="(item , index1) in hangye" @click="changHangye(index1)" :key="index1"
+					 :class="{active:b === index1}" class="shopAdd-cont-list">
+					<span>{{ item.name }}</span>
+				</view>
+			</view>
+		</view>
+		<view class="shopAdd-cont-wrap">
+			<view class="shopAdd-cont-cc">
+				<view class="shopAdd-cont-title">
+					<span>商号区域：</span>
+				</view>
+				
+			</view>
+		</view>
+		<view class="shopAdd-cont-wrap">
+			<view class="shopAdd-cont-cc">
+				<view class="shopAdd-cont-title">
+					<span>成员加入：</span>
+				</view>
+				<view v-for="(item , index2) in chengyuan" @click="changChengyuan(index2)" :key="index2"
+					 :class="{active:c === index2}" class="shopAdd-cont-list-three">
+					<span>{{ item.name }}</span>
+				</view>
+			</view>
+		</view>
+		<view class="shopAdd-cont-wrap">
+			<view class="shopAdd-cont-cc">
+				<view class="shopAdd-cont-title">
+					<span>联系人：</span>
+				</view>
+				<input class="uni-input" placeholder="请填写联系人名称" />
+				<view class="shopAdd-cont-title">
+					<span>联系电话：</span>
+				</view>
+				<input class="uni-input" placeholder="请填写联系电话" />
+				<view class="shopAdd-cont-title">
+					<span>经营地址：</span>
+				</view>
+				<textarea @blur="bindTextAreaBlur" auto-height placeholder="请填写经营地址" class="shopAdd-textarea" />
+			</view>
+		</view>
 
 
 
@@ -34,14 +82,56 @@
 	export default {
 		data() {
 			return {
-				
+				shenfen: [
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'}
+				],
+				hangye: [
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'},
+					{name: '身份'}
+				],
+				chengyuan: [
+					{name: '开放加入'},
+					{name: '审核加入'},
+					{name: '付费加入'}
+				],
+				a: 0,
+				b: 0,
+				c: 0
+		
 			}
+				
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			changShenfen(index) {
+				this.a = index;
+			},
+			changHangye(index1) {
+				this.b = index1;
+			},
+			changChengyuan(index2) {
+				this.c = index2;
+			}
 		}
 	}
 </script>
