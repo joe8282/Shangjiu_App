38b8d179dc9b156e8d9e22128cbc1f580e2687b7
sunlu <645012844@qq.com>
2019-12-25 16:16:02 +0800

    提交

---
+++ pages/shop/shop.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="content">
+		
+	</view>
+	
+</template>
+
+<script>
+	import 'pages/shop/shop.css'
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36px;
+		color: #8f8f94;
+	}
+</style>
