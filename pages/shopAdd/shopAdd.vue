d8a0866aef82d2da2d993e1371c6cb3ac996e390
sunlu <645012844@qq.com>
2019-12-25 09:19:46 +0800

    pagejson

--- pages/shopAdd/shopAdd.vue
+++ pages/shopAdd/shopAdd.vue
@@ -39,7 +39,12 @@
 				<view class="shopAdd-cont-title">
 					<span>商号区域：</span>
 				</view>
-				
+				<view class="content">
+					<span @tap="openAddres">选择区域</span>
+					<!-- <button type="primary" @tap="openAddres">打开地址</button> -->
+					<text class="pickers-select">{{pickerText}}</text>
+					<simple-address ref="simpleAddress" :pickerValueDefault="cityPickerValueDefault" @onConfirm="onConfirm" themeColor='#007AFF'></simple-address>
+				</view>
 			</view>
 		</view>
 		<view class="shopAdd-cont-wrap">
@@ -78,7 +83,8 @@
 </template>
 
 <script>
-	import 'pages/shopAdd/shopAdd.css'
+	import 'pages/shopAdd/shopAdd.css';
+	import simpleAddress from "@/components/simple-address/simple-address.nvue"
 	export default {
 		data() {
 			return {
@@ -114,11 +120,16 @@
 				],
 				a: 0,
 				b: 0,
-				c: 0
+				c: 0,
+				cityPickerValueDefault: [0, 0, 1],
+                pickerText: ''
 		
 			}
 				
 		},
+		components: {
+            simpleAddress
+        },
 		onLoad() {
 
 		},
@@ -131,7 +142,13 @@
 			},
 			changChengyuan(index2) {
 				this.c = index2;
-			}
+			},
+			openAddres() {
+                this.$refs.simpleAddress.open();
+            },
+            onConfirm(e) {
+                this.pickerText = JSON.stringify(e.label)
+            }
 		}
 	}
 </script>
