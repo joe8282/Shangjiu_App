7bedefb29f3c69ab2447c90ea02575436dc60844
szh <434601205@qq.com>
2020-01-09 10:21:43 +0800

    我的代理业务

---
+++ pages/my_dlyw/my_dlyw.vue
@@ -0,0 +1,34 @@
+<template>
+	<view class="body">
+		<view class="content">
+			<view><span>姓  名:</span><label>张冲爱大米</label></view>
+			<view><span>手机号:</span><label>17238456742</label></view>
+			<view><span>加入时间:</span><label>2019-08-07</label></view>
+			<view><span>我的代理商:</span><label>尚祖豪</label></view>
+			<view><span>代理区域:</span><label>江苏省 徐州市 泉山区</label></view>
+		</view>
+		<view class="bk">
+			<view>
+				<span>区域商号</span>
+				<span>205</span>
+			</view>
+			<view class="sx"></view>
+			<view>
+				<span>代理客户</span>
+				<span>200</span>
+			</view>
+			<view class="sx"></view>
+			<view>
+				<span>代理分利</span>
+				<span>￥9999</span>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import 'pages/my_dlyw/my_dlyw.css'
+</script>
+
+<style>
+</style>
