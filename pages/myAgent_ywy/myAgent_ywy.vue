ad986b2a9213a4b3eb480918cf56259fb343f46f
szh <434601205@qq.com>
2020-01-22 11:27:18 +0800

    新增页面

--- pages/myAgent_ywy/myAgent_ywy.vue
+++ pages/myAgent_ywy/myAgent_ywy.vue
@@ -4,6 +4,7 @@
 			<view><span>姓  名:</span><label>张冲爱大米</label></view>
 			<view><span>手机号:</span><label>17238456742</label></view>
 			<view><span>加入时间:</span><label>2019-08-07</label></view>
+			<view><span>我的代理商:</span><label>尚祖豪</label></view>
 			<view><span>代理区域:</span><label>江苏省 徐州市 泉山区</label></view>
 		</view>
 		<view class="bk">
@@ -21,17 +22,12 @@
 				<span>代理分利</span>
 				<span>￥9999</span>
 			</view>
-			<view class="sx"></view>
-			<view>
-				<span>业务员</span>
-				<span>205</span>
-			</view>
 		</view>
 	</view>
 </template>
 
 <script>
-	import 'pages/myAgent/myAgent.css'
+	import 'pages/myAgent_ywy/myAgent_ywy.css'
 </script>
 
 <style>
