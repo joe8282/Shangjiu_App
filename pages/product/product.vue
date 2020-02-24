7105baa7f77ba2280d3803d3e6e7f3516e728eab
sunlu <645012844@qq.com>
2020-02-25 00:07:13 +0800

    productList product

---
+++ pages/product/product.vue
@@ -0,0 +1,234 @@
+<template>
+	<view class="content">
+		<view class="shop-banner-cont">
+			<img src="static/images/shop-banner.jpg" alt="">
+		</view>
+		<view class="shop-title-wrap">
+			<view class="shop-title-cont">
+				<view class="shop-title-ctitle">这是商区标题这是商区标题这是</view>
+				<view class="shop-tx-time-cont">
+					<img src="static/images/my-tx.png" alt="" width="23"/>
+					<span>爱吃面包的猫</span>
+					<label>2019-07-08</label>
+				</view>
+				<view class="shop-ullist">
+					<span>身份</span>
+					<span>类型</span>
+					<span>行业</span>
+					<span>区域</span>
+				</view>
+				<view class="shop-ullist-red">
+					<span>推广（有）</span>
+					<span>免费</span>
+					<span>订阅收费￥20000</span>
+				</view>
+				<view class="shop-ullist-ten">
+					<view class="shop-ullist-ll">
+						<span>评论</span>
+						<label>123</label>
+					</view>
+					<view class="shop-ullist-ll">
+						<span>人气</span>
+						<label>123</label>
+					</view>
+					<view class="shop-ullist-ll">
+						<span>收藏</span>
+						<label>123</label>
+					</view>
+					<view class="shop-ullist-ll">
+						<span>打赏</span>
+						<label>123</label>
+					</view>
+					<view class="shop-ullist-ll">
+						<span>点赞</span>
+						<label>123</label>
+					</view>
+				</view>
+			</view>
+		</view>
+		
+		<view class="shop-six-buttons-cont">
+			<view class="shop-six-buttons-cc shop-four-cc">
+				<span>查看二维码</span>
+			</view>
+		</view>
+		
+		<view class="sssh-wrap">
+			<view class="main-tab-list-cont">
+				<view class="main-tab-nr">
+					<img src="static/images/shangqu-img.png" alt="" class="main-tab-img" />
+					<view class="main-tab-right">
+						<span class="main-tab-r-title">黑乎乎的返回拉萨觉得浪费精力是大家</span>
+						<label class="main-tab-r-ddd">是多久啊来开发时间浪费精力撒旦解放了吉了吉了吉解放了吉</label>
+						<ul class="main-four-ul">
+							<li>身份</li>
+							<li>类型</li>
+							<li>行业</li>
+							<li>区域</li>
+						</ul>
+						<ul class="main-three-ul">
+							<li>免费加入</li>
+							<li>红包（无）</li>
+							<li>体验￥10000</li>
+						</ul>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="sssh-xq-listwrap">
+			<view class="sssh-xq-list">
+				<view class="sssh-xq-listcont">
+					<span>会员可根据需要创建多个商号，用于商家经营推广、创 业、接活；做自媒与内容。商号为创新社群模式，内置 “商讯、商链、说说、问答、</span>
+					<img src="static/images/sxxq-list-img.jpg" alt="" />
+				</view>
+			</view>
+			<view class="sssh-xq-list">
+				<view class="sssh-xq-listcont">
+					<span>会员可根据需要创建多个商号，用于商家经营推广、创 业、接活；做自媒与内容。商号为创新社群模式，内置 “商讯、商链、说说、问答、</span>
+					<img src="static/images/sxxq-list-img.jpg" alt="" />
+				</view>
+			</view>
+			<view class="sssh-xq-list">
+				<view class="sssh-xq-listcont">
+					<span>会员可根据需要创建多个商号，用于商家经营推广、创 业、接活；做自媒与内容。商号为创新社群模式，内置 “商讯、商链、说说、问答、</span>
+					<img src="static/images/sxxq-list-img.jpg" alt="" />
+				</view>
+			</view>
+		</view>
+		
+		<view class="sypj-pj-wrap">
+			<view class="sypj-title">
+				<span>所有评论</span>
+				<label>12358</label>
+			</view>
+			<view class="small-tab small-tab-borderb">
+				<view class="stabl-title-cont">
+					<img src="static/images/fx_tx.png" alt="" height="100%" />
+					<span>胖胖子</span>
+				</view>
+				<view class="stable-dl">
+					成员备注成员备注成员备注成员备注成员备注成员备注成员备注
+				</view>
+				<view class="smtab-imgs smtab-imgs smtab-five">
+					<img src="static/images/shangqu-img.png" alt="">
+					<img src="static/images/shangqu-img.png" alt="">
+					<img src="static/images/shangqu-img.png" alt="">
+					<img src="static/images/shangqu-img.png" alt="">
+					<img src="static/images/shangqu-img.png" alt="">
+				</view>
+				<view class="small-tab-timeer">
+					<i>2019-02-19</i>
+					<view class="small-tab-icons">
+						<span class="icon iconfont">&#xe68b;</span>
+						<label>12580</label>
+					</view>
+					<view class="small-tab-icons">
+						<span class="icon iconfont">&#xe600;</span>
+						<label>12580</label>
+					</view>
+					
+				</view>
+			</view>
+			<view class="small-tab small-tab-borderb">
+				<view class="stabl-title-cont">
+					<img src="static/images/fx_tx.png" alt="" height="100%" />
+					<span>胖胖子</span>
+				</view>
+				<view class="stable-dl">
+					成员备注成员备注成员备注成员备注成员备注成员备注成员备注
+				</view>
+				<view class="smtab-imgs smtab-imgs smtab-five">
+					<img src="static/images/shangqu-img.png" alt="">
+					<img src="static/images/shangqu-img.png" alt="">
+					<img src="static/images/shangqu-img.png" alt="">
+					<img src="static/images/shangqu-img.png" alt="">
+					<img src="static/images/shangqu-img.png" alt="">
+				</view>
+				<view class="small-tab-timeer">
+					<i>2019-02-19</i>
+					<view class="small-tab-icons">
+						<span class="icon iconfont">&#xe68b;</span>
+						<label>12580</label>
+					</view>
+					<view class="small-tab-icons">
+						<span class="icon iconfont">&#xe600;</span>
+						<label>12580</label>
+					</view>
+					
+				</view>
+			</view>
+			<view class="small-tab small-tab-borderb">
+				<view class="stabl-title-cont">
+					<img src="static/images/fx_tx.png" alt="" height="100%" />
+					<span>胖胖子</span>
+				</view>
+				<view class="stable-dl">
+					成员备注成员备注成员备注成员备注成员备注成员备注成员备注
+				</view>
+				<view class="smtab-imgs smtab-imgs smtab-five">
+					<img src="static/images/shangqu-img.png" alt="">
+					<img src="static/images/shangqu-img.png" alt="">
+					<img src="static/images/shangqu-img.png" alt="">
+					<img src="static/images/shangqu-img.png" alt="">
+					<img src="static/images/shangqu-img.png" alt="">
+				</view>
+				<view class="small-tab-timeer">
+					<i>2019-02-19</i>
+					<view class="small-tab-icons">
+						<span class="icon iconfont">&#xe68b;</span>
+						<label>12580</label>
+					</view>
+					<view class="small-tab-icons">
+						<span class="icon iconfont">&#xe600;</span>
+						<label>12580</label>
+					</view>
+					
+				</view>
+			</view>
+		</view>
+		
+		
+		
+		
+		
+		
+	</view>
+	
+</template>
+
+<script>
+	import 'pages/product/product.css';
+	import uniFav from "@/components/uni-fav/uni-fav.vue"
+	export default {
+		components: {
+			uniFav
+		},
+		data() {
+			return {				　
+				
+				
+				
+			}
+		}
+
+		
+		
+
+		
+	}
+</script>
+
+<style>
+	
+
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36px;
+		color: #8f8f94;
+	}
+</style>
---
+++ pages/productList/productList.vue
@@ -0,0 +1,297 @@
+<template>
+	<view class="content" id="app">
+		<view class="text-area">
+			<view class="main-header-wrap">
+				<view class="uni-form-item uni-column">
+					<img src="static/images/search.png" alt="" class="search-icon-img" />
+					<input class="uni-input"  placeholder="      输入商号相关信息" />
+				</view>
+				<img src="static/images/news.png" alt="" class="main-header-news-icon" />
+			</view>
+			<view class="swiper-wrap">
+				<uni-swiper-dot :info="info" :current="current" :mode="mode" :dots-styles="dotsStyles" field="content">
+					<swiper class="swiper-box" @change="change">
+						<swiper-item v-for="(item, index) in info" :key="index">
+							<view :class="item.colorClass" class="swiper-item">
+								<image class="image" :src="item.url" mode="aspectFill" width="100%" />
+							</view>
+						</swiper-item>
+					</swiper>
+				</uni-swiper-dot>
+			</view>
+			<view class="productList-slider-wrap">
+				<view class="productList-slider-three">
+					<view class="productList-ssthree">
+						<img src="static/images/all1.png" alt="" />
+						<span>全部</span>
+					</view>
+					<view class="productList-ssthree">
+						<img src="static/images/all2.png" alt="" />
+						<span>免费</span>
+					</view>
+					<view class="productList-ssthree">
+						<img src="static/images/all3.png" alt="" />
+						<span>付费</span>
+					</view>
+				</view>
+				<view class="productList-slider-toggle">
+					<view class="ps-toggle-list">
+						<view class="pstoggle-list-left">
+							<b>类型 |</b>
+						</view>
+						<view class="stoggle-list-right" id="hideshow-slider">
+							<span>社团</span>
+							<span>全部</span>
+							<span>商报</span>
+							<span>商社</span>
+							<span>资讯</span>
+							<span>社团</span>
+							<span>全部</span>
+							<span>商报</span>
+							<span>商社</span>
+							<span>资讯</span>
+						</view>
+						<img src="static/images/down-icon.png" class="slider-down-icon" @click="sliderToggle()":class="[rotate?  'rotatetop' : 'rotatebottom']"/>
+					</view>
+					<view class="ps-toggle-list">
+						<view class="pstoggle-list-left">
+							<b>应用 |</b>
+						</view>
+						<view class="stoggle-list-right" id="hideshow-slider1">
+							<span>全部</span>
+							<span>商社</span>
+							<span>商报</span>
+							<span>社团</span>
+							<span>资讯</span>
+							<span>全部</span>
+							<span>商社</span>
+							<span>商报</span>
+							<span>社团</span>
+							<span>资讯</span>
+						</view>
+						<img src="static/images/down-icon.png" class="slider-down-icon" @click="sliderToggle1()":class="[rotate1?  'rotatetop' : 'rotatebottom']"/>
+					</view>
+					<view class="ps-toggle-list">
+						<view class="pstoggle-list-left">
+							<b>行业 |</b>
+						</view>
+						<view class="stoggle-list-right" id="hideshow-slider2">
+							<span>全部</span>
+							<span>商社</span>
+							<span>商报</span>
+							<span>社团</span>
+							<span>资讯</span>
+							<span>全部</span>
+							<span>商社</span>
+							<span>商报</span>
+							<span>社团</span>
+							<span>资讯</span>
+						</view>
+						<img src="static/images/down-icon.png" class="slider-down-icon" @click="sliderToggle2()":class="[rotate2?  'rotatetop' : 'rotatebottom']"/>
+					</view>
+					<view class="ps-toggle-list">
+						<view class="pstoggle-list-left">
+							<b>排序 |</b>
+						</view>
+						<view class="stoggle-list-right" id="hideshow-slider3">
+							<span>综合</span>
+							<span>浏览</span>
+							<span>时间</span>
+							<span>关注</span>
+							<span>成员</span>
+							<span>综合</span>
+							<span>浏览</span>
+							<span>时间</span>
+							<span>关注</span>
+							<span>成员</span>
+						</view>
+						<img src="static/images/down-icon.png" class="slider-down-icon" @click="sliderToggle3()":class="[rotate3?  'rotatetop' : 'rotatebottom']"/>
+					</view>
+				</view>
+			</view>
+			<view class="productlist-hh-wrap">
+				<view class="productlist-hh-list">
+					<view class="small-tab">
+						<view class="main-tab-nr">
+							<img src="static/images/shangqu-img.png" alt="" class="main-tab-img" />
+							<view class="main-tab-right">
+								<span class="main-tab-r-title">黑乎乎的返回拉萨觉得浪费精力是大家</span>
+								<label class="main-tab-r-ddd">是多久啊来开发时间浪费精力撒旦解放了吉了吉了吉解放了吉</label>
+								<ul class="main-four-ul">
+									<li>身份</li>
+									<li>类型</li>
+									<li>行业</li>
+									<li>区域</li>
+									<span class="dysf-style">订阅收费 ￥10000</span>
+								</ul>
+							</view>
+						</view>
+					</view>
+				</view>
+				<view class="productlist-hh-list">
+					<view class="small-tab">
+						<view class="main-tab-nr">
+							<img src="static/images/shangqu-img.png" alt="" class="main-tab-img" />
+							<view class="main-tab-right">
+								<span class="main-tab-r-title">黑乎乎的返回拉萨觉得浪费精力是大家</span>
+								<label class="main-tab-r-ddd">是多久啊来开发时间浪费精力撒旦解放了吉了吉了吉解放了吉</label>
+								<ul class="main-four-ul">
+									<li>身份</li>
+									<li>类型</li>
+									<li>行业</li>
+									<li>区域</li>
+									<span class="dysf-style">订阅收费 ￥10000</span>
+								</ul>
+							</view>
+						</view>
+					</view>
+				</view>
+				<view class="productlist-hh-list">
+					<view class="small-tab">
+						<view class="main-tab-nr">
+							<img src="static/images/shangqu-img.png" alt="" class="main-tab-img" />
+							<view class="main-tab-right">
+								<span class="main-tab-r-title">黑乎乎的返回拉萨觉得浪费精力是大家</span>
+								<label class="main-tab-r-ddd">是多久啊来开发时间浪费精力撒旦解放了吉了吉了吉解放了吉</label>
+								<ul class="main-four-ul">
+									<li>身份</li>
+									<li>类型</li>
+									<li>行业</li>
+									<li>区域</li>
+									<span class="dysf-style">订阅收费 ￥10000</span>
+								</ul>
+							</view>
+						</view>
+					</view>
+				</view>
+			</view>
+			
+			
+		</view>
+		
+	
+	</view>
+	
+</template>
+
+<script>
+	import 'pages/productList/productList.css';
+	import uniSection from '@/components/uni-section/uni-section.vue';
+	import uniSwiperDot from "@/components/uni-swiper-dot/uni-swiper-dot.vue";
+	import uniFav from "@/components/uni-fav/uni-fav.vue"
+	export default {
+		components: {
+			uniFav,
+			uniSection,
+			uniSwiperDot
+		},
+		data() {
+			return {
+				sss:false,
+				rotate: false,
+				rotate1: false,
+				rotate2: false,
+				rotate3: false,
+				info: [{
+						colorClass: 'uni-bg-red',
+						url: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/shuijiao.jpg',
+						content: '内容 A'
+					},
+					{
+						colorClass: 'uni-bg-green',
+						url: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/muwu.jpg',
+						content: '内容 B'
+					},
+					{
+						colorClass: 'uni-bg-blue',
+						url: 'https://img-cdn-qiniu.dcloud.net.cn/uniapp/images/cbd.jpg',
+						content: '内容 C'
+					}
+				],
+				dotStyle: [{
+						backgroundColor: 'rgba(0, 0, 0, .3)',
+						border: '1px rgba(0, 0, 0, .3) solid',
+						color: '#fff',
+						selectedBackgroundColor: 'rgba(0, 0, 0, .9)',
+						selectedBorder: '1px rgba(0, 0, 0, .9) solid'
+					},
+					{
+						backgroundColor: 'rgba(255, 90, 95,0.3)',
+						border: '1px rgba(255, 90, 95,0.3) solid',
+						color: '#fff',
+						selectedBackgroundColor: 'rgba(255, 90, 95,0.9)',
+						selectedBorder: '1px rgba(255, 90, 95,0.9) solid'
+					},
+					{
+						backgroundColor: 'rgba(83, 200, 249,0.3)',
+						border: '1px rgba(83, 200, 249,0.3) solid',
+						color: '#fff',
+						selectedBackgroundColor: 'rgba(83, 200, 249,0.9)',
+						selectedBorder: '1px rgba(83, 200, 249,0.9) solid'
+					}
+				],
+				modeIndex: -1,
+				styleIndex: -1,
+				current: 0,
+				mode: 'default',
+				dotsStyles: {}
+				　
+				
+
+			}
+		},
+		onLoad() {},
+		methods: {
+			change(e) {
+				this.current = e.detail.current
+			},
+			selectStyle(index) {
+				this.dotsStyles = this.dotStyle[index]
+				this.styleIndex = index
+			},
+			selectMode(mode, index) {
+				this.mode = mode
+				this.modeIndex = index
+				this.styleIndex = -1
+				this.dotsStyles = this.dotStyle[0]
+			},
+			sliderToggle() {
+			    if(this.rotate == false){
+			        document.getElementById("hideshow-slider").style.height="auto"
+					this.rotate = !this.rotate;
+			    }else{
+			        document.getElementById("hideshow-slider").style.height="24px"
+					this.rotate = !this.rotate;
+			    }
+			},
+			sliderToggle1() {
+			    if(this.rotate1 == false){
+			        document.getElementById("hideshow-slider1").style.height="auto"
+					this.rotate1 = !this.rotate1;
+			    }else{
+			        document.getElementById("hideshow-slider1").style.height="24px"
+					this.rotate1 = !this.rotate1;
+			    }
+			},
+			sliderToggle2() {
+			    if(this.rotate2 == false){
+			        document.getElementById("hideshow-slider2").style.height="auto"
+					this.rotate2 = !this.rotate2;
+			    }else{
+			        document.getElementById("hideshow-slider2").style.height="24px"
+					this.rotate2 = !this.rotate2;
+			    }
+			},
+			sliderToggle3() {
+			    if(this.rotate3 == false){
+			        document.getElementById("hideshow-slider3").style.height="auto"
+					this.rotate3 = !this.rotate3;
+			    }else{
+			        document.getElementById("hideshow-slider3").style.height="24px"
+					this.rotate3 = !this.rotate3;
+			    }
+			}
+		}
+	}
+</script>
+
